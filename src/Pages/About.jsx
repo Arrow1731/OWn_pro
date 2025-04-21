3e59c83b47331b43dba1362142ab7ace1a52f5a5
arrow1701 <azizbekkamilov1701@gmail.com>
2025-04-21 11:38:43 +0500

    what about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react'
 export default class About extends Component {
   render() {
     return (
-      <div className='container'>About</div>
+      <div className='container'>
+        <h1>Salom Abutga nima yozaman Bro</h1>
+      </div>
     )
   }
 }
